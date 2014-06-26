c3e6bd27c9d8342cf7d3efbfa4a698d0a32011ba
Richard Hitier <richard.hitier@co-libri.org>
2014-06-26 15:08:24 +0200

    init

---
+++ makina.js
@@ -0,0 +1,103 @@
+var z=13;
+var myLL= L.latLng(40.59, 1.45);
+
+function init(){
+
+  var southWest = L.latLng(43, 1),
+      northEast = L.latLng(44, 2),
+      bounds = L.latLngBounds(southWest, northEast);
+
+  // create a map in the "map" div, set the view to a given place and zoom
+  //var map = L.map('map').setView([y, x], z);
+    var map = L.map('map', {
+//      minZoom: 8,
+//      maxZoom: 11,
+//      maxBounds: bounds,
+      center:myLL,
+      zoom:z});
+
+  // add an OpenStreetMap tile layer
+  var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'OpenStreetMap'
+      });
+  osmLayer.addTo(map);
+  var mapqLayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+      attribution: 'MapQuest OpenStreetMap',
+      subdomains: ['otile1','otile2','otile3','otile4']
+      });
+  mapqLayer.addTo(map);
+
+  var geofLayer = L.tileLayer.wms("https://api.geofoncier.fr:443/referentielsoge/ogc/wxs?SERVICE=WMS",{
+      layers: 'DOSSIERS_FXX',
+      format: 'image/png',
+      transparent: true,
+      attribution: 'geofoncier'
+      });
+  geofLayer.addTo(map);
+
+  //      .openPopup();
+  var mark1 = L.marker([ 43.59, 1.45]).bindPopup('mark1');
+  var mark2 = L.marker([ 43.49, 1.55]).bindPopup('mark2');
+  var mark3 = L.marker([ 43.39, 1.65]).bindPopup('mark3');
+  var mark4 = L.marker([ 43.29, 1.75]).bindPopup('mark4');
+  var markGroup = L.layerGroup( [mark1,mark2,mark3,mark4]);
+
+
+
+  var elmOptions = {
+      radius: 8,
+      fillColor: "red",
+      color: "black",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
+  var matOptions = {
+      radius: 8,
+      fillColor: "yellow",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
+
+  var elmLayer = L.geoJson(  ecolesElm, {
+      pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, elmOptions);
+      },
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.Ecole);
+    }
+  });
+
+  var matLayer = L.geoJson(  ecolesMat, {
+      pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, matOptions);
+      },
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.Ecole);
+    }
+  });
+
+  var baseLayers = {
+    "MapBox": mapqLayer,
+    "OSM": osmLayer
+  };
+
+  var overlays = {
+    "elementaires": elmLayer,
+    "maternelles": matLayer,
+    "geofoncier": geofLayer,
+    "marqueurs": markGroup,
+  };
+
+  L.control.layers(baseLayers, overlays).addTo(map);
+
+
+
+
+}
+
+
+init();
+
